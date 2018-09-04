fadabd9d00715471bd0fb2721a9bf7407d36e5c7
ombarus <ombarus.dev@gmail.com>
2018-09-04 15:26:29 +0900

    - Options page title should show the throw ("2d6's options") - Final roll page should have a usefull title ("2d6 + 3 reroll" ?) - show crit miss as % in results - flush history once in a while - fix history save/load of save_data - Roll more than one dice per "session" - Implement mersene twister in javascript

--- js/my-app.js
+++ js/my-app.js
@@ -1,3 +1,6 @@
+
+var twister = new MersenneTwister();
+
 var app = new Framework7({
 	root: '#app',
     routes: routes,
@@ -8,30 +11,32 @@ var app = new Framework7({
 var mainView = app.views.create('.view-main');
 var $$ = Dom7;
 
-
+// This is not ever used except as a reference, it might not be up to date (but I try)
 var sample_data = JSON.parse('{ '+
+'	"version":0, ' +
 '	"current_roll": [ '+
-'		{"roll_data":{"dice":6,"count":3, "max":true, "one":false, "bonus":-2, "bonus_roll":0}, "results":[1,5,12]}, '+
-'		{"roll_data":{"dice":8,"count":5, "max":false, "one":false, "bonus":0, "bonus_roll":0}, "results":[1,2,8,4,5]} '+
+'		{"roll_data":{"dice":6,"count":3, "max":true, "min":false, "bonus":-2, "bonus_roll":0}, "results":[1,5,12]}, '+
+'		{"roll_data":{"dice":8,"count":5, "max":false, "min":false, "bonus":0, "bonus_roll":0}, "results":[1,2,8,4,5]} '+
 '	], '+
 '	"history":[ '+
 '		[ '+
-'			{"roll_data":{"dice":6,"count":3, "max":true, "one":false, "bonus":-2, "bonus_roll":0}, "results":[1,5,12]}, '+
-'			{"roll_data":{"dice":8,"count":5, "max":false, "one":false, "bonus":0, "bonus_roll":0}, "results":[1,2,8,4,5]} '+
+'			{"roll_data":{"dice":6,"count":3, "max":true, "min":false, "bonus":-2, "bonus_roll":0}, "results":[1,5,12]}, '+
+'			{"roll_data":{"dice":8,"count":5, "max":false, "min":false, "bonus":0, "bonus_roll":0}, "results":[1,2,8,4,5]} '+
 '		], '+
 '		[ '+
-'			{"roll_data":{"dice":8,"count":5, "max":false, "one":false, "bonus":0, "bonus_roll":0}, "results":[1,2,8,4,5]} '+
+'			{"roll_data":{"dice":8,"count":5, "max":false, "min":false, "bonus":0, "bonus_roll":0}, "results":[1,2,8,4,5]} '+
 '		], '+
 '		[ '+
-'			{"roll_data":{"dice":6,"count":20, "max":true, "one":false, "bonus":-2, "bonus_roll":0}, "results":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]} '+
+'			{"roll_data":{"dice":6,"count":20, "max":true, "min":false, "bonus":-2, "bonus_roll":0}, "results":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]} '+
 '		] '+
 '	], '+
 '	"presets":[ '+
 '		{ '+
-'			"roll_data":[{"dice":6,"count":3, "max":true, "one":false, "bonus":-2, "bonus_roll":0},{"dice":8,"count":5, "max":false, "one":false, "bonus":0, "bonus_roll":0}], '+
+'			"roll_data":[{"dice":6,"count":3, "max":true, "min":false, "bonus":-2, "bonus_roll":0},{"dice":8,"count":5, "max":false, "min":false, "bonus":0, "bonus_roll":0}], '+
 '			"name":"1d6+3 spellcasting" '+
 '		} '+
 ']}');
+///////
 
 $$("#dice-side").on("formajax:success", function() {
 	var val = $$("input[name='dice-side-input']").val();
@@ -74,10 +79,36 @@ function ProcessClick(ev) {
 		save_data.current_roll[save_data.current_roll.length-1].roll_data.bonus_dice = parseInt(dicebonus);
 		save_data.current_roll[save_data.current_roll.length-1].roll_data.bonus_roll = parseInt(rollbonus);
 		
-		save_data.current_roll[save_data.current_roll.length-1].results = Roll(save_data.current_roll[save_data.current_roll.length-1].roll_data);
-		save_data.history.push(save_data.current_roll[save_data.current_roll.length-1]);
+		for (var i = 0; i < save_data.current_roll.length; i++) {
+			save_data.current_roll[i].results = Roll(save_data.current_roll[i].roll_data);	
+		}
+		AddHistory(save_data.current_roll);
 		app.form.storeFormData("save.json", save_data);
 		console.log(save_data);
+	}
+	if (btn.attr('id') == "addroll")
+	{
+		var rerollmax = $$("input[name='dice-reroll-max']").is(':checked');
+		var rerollmin = $$("input[name='dice-reroll-min']").is(':checked');
+		var dicebonus = $$("input[name='dice-bonus']").val();
+		var rollbonus = $$("input[name='roll-bonus']").val();
+		
+		save_data.current_roll[save_data.current_roll.length-1].roll_data.max = rerollmax;
+		save_data.current_roll[save_data.current_roll.length-1].roll_data.min = rerollmin;
+		save_data.current_roll[save_data.current_roll.length-1].roll_data.bonus_dice = parseInt(dicebonus);
+		save_data.current_roll[save_data.current_roll.length-1].roll_data.bonus_roll = parseInt(rollbonus);
+		
+		save_data.current_roll.push({"roll_data":{}, "results":[]});
+		app.form.storeFormData("save.json", save_data);
+		console.log(save_data);
+	}
+}
+
+function AddHistory(data) {
+	var max_history = 20;
+	save_data.history.push(JSON.parse(JSON.stringify(data))); // a strange way to deep clone the roll instead of copying the reference
+	if (save_data.history.length > max_history) {
+		save_data.history.splice(0, save_data.history.length-max_history);
 	}
 }
 
@@ -107,10 +138,7 @@ function Roll(rolldata) {
 }
 
 function MyRand(max) {
-	// TODO: Use Mersene Twister, do NOT trust javascript random method... urk
-	var roll = Math.round((Math.random() * max)) + 1;
-	//////////////////////////////////////////////////////////////////////////
-	
+	var roll = twister.genrand_max(max);
 	return roll;
 }
 
@@ -213,17 +241,15 @@ function GetCrit() {
 	return totalcrit;
 }
 
-function MersenneTwister() {
-	
-}
-
 function GetMiss() {
 	var i = 0;
 	var totalmiss = 0;
+	var totaldice = 0;
 	for (i = 0; i < save_data.current_roll.length; i++)
 	{
 		var inner = save_data.current_roll[i];
 		var j = 0;
+		totaldice += inner.results.length;
 		for (j = 0; j < inner.results.length; j++)
 		{
 			if (inner.results[j] - inner.roll_data.bonus_dice == 1)
@@ -232,7 +258,7 @@ function GetMiss() {
 			}
 		}
 	}
-	return totalmiss;
+	return totalmiss + " (" + (totalmiss / totaldice * 100).toFixed(0) + "%)";
 }
 
 function GetThresholdCount(threshold) {
@@ -280,6 +306,66 @@ function UpdateSumText() {
 	}
 	
 	$$("#stats-sum-title").text(sumtext);
+}
+
+function UpdateDiceTitle() {
+	var title = "";
+	if (save_data.current_roll.length > 1) {
+		title = "Multiple Roll";
+	}
+	else {
+		var dicetype = save_data.current_roll[0].roll_data.dice;
+		var dicecount = save_data.current_roll[0].roll_data.count;
+		var bonus_dice = save_data.current_roll[0].roll_data.bonus_dice;
+		var bonus_roll = save_data.current_roll[0].roll_data.bonus_roll;
+		var reroll_max = save_data.current_roll[0].roll_data.max;
+		var reroll_one = save_data.current_roll[0].roll_data.min;
+		
+		title += dicecount + "D"
+		if (bonus_dice != 0) {
+			title += "(";
+		}
+		title += dicetype;
+		if (bonus_dice != 0) {
+			if (bonus_dice > 0) {
+				title += "+";
+			}
+			title += bonus_dice + ")";
+		}
+		if (bonus_roll != 0) {
+			title += " ";
+			if (bonus_roll > 0) {
+				title += "+ ";
+			}
+			title += bonus_roll;
+		}
+		if (reroll_max == true || reroll_one == true) {
+			title += " Reroll ";
+			if (reroll_max == true) {
+				title += dicetype;
+			}
+			if (reroll_max == true && reroll_one) {
+				title += "/";
+			}
+			if (reroll_one == true) {
+				title += "1";
+			}
+		}
+	}
+	
+	$$("#title-results").text(title);
+}
+
+function UpdateOptionsTitle() {
+	var title = "";
+	
+	var dicetype = save_data.current_roll[0].roll_data.dice;
+	var dicecount = save_data.current_roll[0].roll_data.count;
+	
+	title += dicecount + "D" + dicetype;	
+	
+	title += "'s Options";
+	$$("#title-options").text(title);
 }
 
 function UpdateDiceList() {
@@ -335,9 +421,9 @@ function UpdateDiceList() {
 	$$("#dice-list").append(content);
 }
 
-var initial_data = JSON.parse('{"current_roll":[{"roll_data":{}, "results":{}}], "history":[], "presets":[]}');
+var initial_data = JSON.parse('{"version":1, "current_roll":[{"roll_data":{}, "results":[]}], "history":[], "presets":[]}');
 save_data = app.form.getFormData("save.json");
-if (save_data == null)
+if (save_data == null || save_data.version == undefined || save_data.version != initial_data.version)
 {
 	save_data = initial_data;
 	app.form.storeFormData("save.json", initial_data);
@@ -349,8 +435,8 @@ else
 
 $$(document).on('page:init', function (e) {
 	
-	app.form.storeFormData("save.json", initial_data);
-	var data = app.form.getFormData("save.json");
+	//app.form.storeFormData("save.json", initial_data);
+	//var data = app.form.getFormData("save.json");
 	
     $$('#threshold-slider').on('touchstart', function(event) {
 		app.swiper.get($$('.tabs-swipeable-wrap')).allowTouchMove = false;
@@ -390,6 +476,10 @@ $$(document).on('page:init', function (e) {
 		ProcessClick(ev);
 	});
 	
+	if ($$("#title-options").length != 0) {
+		UpdateOptionsTitle();
+	}
+	
 	if ($$("#stats-count").length != 0)
 	{
 		$$("#stats-count").text(GetCount());
@@ -406,5 +496,6 @@ $$(document).on('page:init', function (e) {
 		$$("#stats-threshold-count").text(GetThresholdCount($$("#stats-threshold-range")[0].value));
 		
 		UpdateDiceList();
+		UpdateDiceTitle();
 	}
 });
